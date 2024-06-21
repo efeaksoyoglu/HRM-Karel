545abc512109f508ec773ea5ac6fa2dd940dd6fb
Atila Efe AKSOYOĞLU <100439200+efeaksoyoglu@users.noreply.github.com>
2024-06-21 16:42:46 +0300

    Add files via upload
    Alınan yıllık/mazeret izninin döküman raporunun alınmasını sağlayan butonun, yönlendirme yapmasını sağlayan kod
    (not: koddaki web bağlantı linki, veri koruma açısından değiştirilerek paylaşılmıştır.)

---
+++ izin yazdır.js
@@ -0,0 +1,12 @@
+if(apex.item('P97_ROW_DELETE_2').getValue()=='Y')
+{
+    console.log(apex.item('P97_ROW_DELETE_2').getValue());
+javascript:window.open('http://r12srv.karel.com.tr:/reports/rwserv' +apex.item('P97_ROW_DELETE_1').getValue());
+
+}
+else
+{
+        console.log(apex.item('P97_ROW_DELETE_2').getValue());
+    javascript:window.open('http://r12srv.karel.com.tr:/reports/rwserv' +apex.item('P97_ROW_DELETE_1').getValue());
+
+}
